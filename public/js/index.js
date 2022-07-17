9bfe19c93db0176215fd81d4ee94bba414a82216
Ramkumar S <srdhrramkumar@gmail.com>
2022-07-17 21:48:45 +0530

    weather app init commit

---
+++ public/js/index.js
@@ -0,0 +1,14 @@
+
+document.querySelector('form').addEventListener('submit',(event) => {
+    event.preventDefault();
+    const location = event.target.location.value;
+    if(location != ''){
+        fetch(`http://localhost:3000/weather?place=${location}`).then((response) => {
+            response.json().then(data => {
+                document.getElementById('response').innerText = data.msg;
+            });
+        });
+    }else{
+        document.getElementById('response').innerText = 'Please provide a location';
+    }
+});
